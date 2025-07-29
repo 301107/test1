9b3fd98d1644ccf20eac437a236f14450038c696
Герман Жукьян <6764319@gmail.com>
2025-07-29 11:21:43 +0400

    last commit for today

---
+++ README.md
@@ -0,0 +1,1 @@
+Hello, there will be python code and useful powershell scripts
---
+++ script.ps1
@@ -0,0 +1,3 @@
+Write-Host "Hello from PowerShell in VS Code!"
+$currentDate = Get-Date
+Write-Host "Today's date is: $currentDate"
